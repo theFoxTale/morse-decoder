db0fe6ff975622ce7b9631f1332a3761a4d79e97
Макаренко Анна Николаевна <makarenko-an@part-kom.ru>
2025-09-08 01:11:31 +0300

    init: add morse decoder code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,34 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const letters = [];
+  const blockSize = 10;
+  for (let i = 0; i < expr.length; i += blockSize) {
+    letters.push(expr.slice(i, i + blockSize));
+  }
+
+  const lastPart = expr.length % blockSize;
+  if (lastPart > 0) {
+    letters.push(expr.slice(-lastPart));
+  }
+
+  const res = [];
+  for (let i = 0; i < letters.length; i += 1) {
+    const letter = letters[i];
+    if (letter === '**********') {
+      res.push(' ');
+    } else {
+      const morse = [];
+      for (let j = 0; j < letter.length; j += 2) {
+        const code = letter.slice(j, j + 2);
+        if (code === '10') morse.push('.');
+        if (code === '11') morse.push('-');
+      }
+      res.push(MORSE_TABLE[morse.join('')]);
+    }
+  }
+
+  return res.join('');
 };
